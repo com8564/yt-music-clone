193ba53a2ed38e442e818bad23757ba0cabb06c2
Kyoung Yoon Ham <com8564@naver.com>
2024-08-10 18:06:04 +0900

    PagePadding component

--- app/channel/[id]/layout.tsx
+++ app/channel/[id]/layout.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 
-const layout = ({ children }) => {
-  return (
-    <div>
-      channel layout
-      {children}
-    </div>
-  );
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
 };
 
 export default layout;
--- app/explore/layout.tsx
+++ app/explore/layout.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 
-const layout = ({ children }) => {
-  return (
-    <div>
-      explore layout
-      {children}
-    </div>
-  );
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
 };
 
 export default layout;
