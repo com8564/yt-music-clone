e10db224c1356a4f6f69e82871fcc7d5a068c875
Kyoung Yoon Ham <com8564@naver.com>
2024-08-11 21:05:01 +0900

    Sidebar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,48 @@
+"use client";
+
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import UserIcon from "@/components/UserIcon";
 import PagePadding from "@/components/PagePadding";
+import { FaChromecast } from "react-icons/fa";
+import { FiSearch } from "react-icons/fi";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import Logo from "@/components/elements/Logo";
+import Navigator from "./elements/Navigator";
+
+const HeaderDrawer = ({ children }) => {
+  const [isopen, setIsOpen] = useState(false);
+
+  return (
+    <Drawer direction="left" open={isopen} onOpenChange={setIsOpen}>
+      <DrawerTrigger>{children}</DrawerTrigger>
+      <DrawerContent className="w-[240px] h-full">
+        {/* 로고 */}
+        {/* 네비게이션, 재싱목록 */}
+        <div className="py-3">
+          <div className="px-3">
+            <Logo
+              isInDrawer={true}
+              onClickClose={() => {
+                setIsOpen(false);
+              }}
+            ></Logo>
+          </div>
+          <Navigator />
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+};
 
 const Header = ({ children }) => {
   return (
@@ -22,8 +63,27 @@ const Header = ({ children }) => {
       {/* search section */}
       <section className="sticky">
         <PagePadding>
-          searchSection
-          <UserIcon></UserIcon>
+          <div className="h-[64px] flex fles-row justify-between items-center">
+            <article className="hidden lg:flex flex-row items-center h-[42px] min-w-[480px] bg-[rgba(0,0,0,0.14)] rounded-2xl px-[16px] gap-[16px]">
+              <div>
+                <FiSearch size={24}></FiSearch>
+              </div>
+              <input
+                type="text"
+                className="h-full w-full bg-transparent"
+                placeholder="노래, 앨범, 아티스트, 팟캐스트 검색"
+              />
+            </article>
+            <HeaderDrawer>
+              <article className="lg:hidden">
+                <Logo />
+              </article>
+            </HeaderDrawer>
+            <article className="flex flex-row gap-6 items-center">
+              <FaChromecast size={26} />
+              <UserIcon></UserIcon>
+            </article>
+          </div>
         </PagePadding>
       </section>
       <section className="relative">{children}</section>
--- components/elements/Logo.jsx
+++ components/elements/Logo.jsx
@@ -4,19 +4,29 @@ import { useRouter } from "next/navigation";
 import React from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import IconButton from "./IconButton";
-const Logo = () => {
+import { IoCloseOutline } from "react-icons/io5";
+
+const Logo = ({ isInDrawer = false, onClickClose = () => {} }) => {
   const { push } = useRouter();
 
   const onClickLogo = () => {
     push("/");
   };
   const onClickMenu = () => {};
+
   return (
     <section className="flex flex-row items-center gap-3">
-      <IconButton
-        onCllickIcon={onClickMenu}
-        icon={<RxHamburgerMenu size={24} />}
-      />
+      {isInDrawer ? (
+        <IconButton
+          onCllickIcon={onClickClose}
+          icon={<IoCloseOutline size={30} />}
+        />
+      ) : (
+        <IconButton
+          onCllickIcon={onClickMenu}
+          icon={<RxHamburgerMenu size={24} />}
+        />
+      )}
       <div className="cursor-pointer" onClick={onClickLogo}>
         <Image alt="logo" width={100} height={30} src={"/main-logo.svg"} />
       </div>
